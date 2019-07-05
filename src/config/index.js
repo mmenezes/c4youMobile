f62f094b1cd2ed9e2d5510131cf512e18894380e
Eric Pereira <wwwescape@gmail.com>
2019-07-05 10:47:28 +0530

    Updated common API URL

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 export const DOCUMENT_ROOT = '/';
 export const LOGIN_API_URL = 'http://localhost:7003/api/';
-export const API_URL = 'http://localhost:5000/api/';
+export const API_URL = 'https://care4u-callforcode-psl.eu-gb.mybluemix.net/';
 export const WATSON_API_URL = 'http://localhost:3000/';
